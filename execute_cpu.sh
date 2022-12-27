0c52a6ee648ac50e70570169bb17267f9cfca87e
xvyang <62140017+serein-dream@users.noreply.github.com>
2022-12-27 10:14:13 +0000

    Delete execute_cpu.sh

--- execute_cpu.sh
+++
@@ -1,9 +0,0 @@
-python train.py --data_dir ./data/ \
-                --dataset mnist \
-                --device cpu \
-                --world_size 3 \
-                --batch_size 150 \
-                --lr 0.01 \
-                --lr_gamma 0.9 \
-                --epoch 200 \
-                --backend gloo
